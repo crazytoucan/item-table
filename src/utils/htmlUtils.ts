ffb8fe67d8f9ab417f228bbf527594b4f21cf5d2
Alex Ryan <alexanderfryan@gmail.com>
2020-09-25 15:09:47 -0400

    max-classes-per-file: false

--- src/utils/htmlUtils.ts
+++ src/utils/htmlUtils.ts
@@ -1,5 +1,5 @@
 export function setStyle(el: HTMLElement, style: Partial<CSSStyleDeclaration>) {
   for (const key of Object.keys(style)) {
-    el.style[key] = style[key];
+    el.style[key as any] = style[key as any]!;
   }
 }
